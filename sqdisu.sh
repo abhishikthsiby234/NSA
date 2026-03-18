2483cb6050c992def26e3e35babf995e2ce63ae8
abhishikthsiby234 <abhishikthsiby234@gmail.com>
2026-03-18 15:00:33 +0530

    Add files via upload

---
+++ sqdisu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "enter a number :"
+read num
+sum=0
+while [ $num -gt 0 ]
+do
+	digit=$((num % 10))
+	square=$((digit * digit))
+	sum=$((sum + square))
+	num=$((num / 10))
+done
+echo "sum of squares of digit = $sum"
