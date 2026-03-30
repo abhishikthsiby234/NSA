b54a2ebfd1b23e0eb00d515771de5e8363092472
abhishikthsiby234 <abhishikthsiby234@gmail.com>
2026-03-30 14:01:04 +0530

    Implement sum_of_digits function in digitsumfunction.sh
    Add a function to calculate the sum of digits of a number.

---
+++ digitsumfunction.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+ 
+sum_of_digits() {
+    num=$1
+    sum=0
+
+    while [ $num -gt 0 ]
+    do
+        digit=$((num % 10))
+        sum=$((sum + digit))
+        num=$((num / 10))
+    done
+
+    echo $sum
+}
+
+
+echo "Enter a number:"
+read number
+
