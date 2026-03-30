f56451951f994f90b5f8ab762deaf0816b8a1505
abhishikthsiby234 <abhishikthsiby234@gmail.com>
2026-03-30 14:26:14 +0530

    Add script to generate combinations of 1, 2, 3

---
+++ combinations.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Program 25: Generate Combinations of 1, 2, 3"
+for i in 1 2 3; do
+    for j in 1 2 3; do
+        for k in 1 2 3; do
+            echo "$i$j$k"
+        done
+    done
+done
+echo
